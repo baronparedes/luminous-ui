a8b78f4eb6f21edbe74bd24278229010002ddeef
Baron Paredes <baronpatrick.paredes@gmail.com>
2021-10-28 23:46:06 +0800

    feat(notify_approvers)

--- src/components/expense/actions/NotifyApprovers.tsx
+++ src/components/expense/actions/NotifyApprovers.tsx
@@ -1,4 +1,8 @@
+import {useState} from 'react';
 import {Button, ButtonProps} from 'react-bootstrap';
+import {FaCheck} from 'react-icons/fa';
+
+import {useNotifyApprovers} from '../../../Api';
 
 type Props = {
   purchaseOrderId: number;
@@ -10,14 +14,22 @@ const NotifyApprovers = ({
   buttonLabel,
   ...buttonProps
 }: Props & ButtonProps) => {
+  const [notified, setNotified] = useState(false);
+  const {mutate, loading} = useNotifyApprovers({id: purchaseOrderId});
+
   const handleOnClick = () => {
-    console.log(purchaseOrderId);
+    mutate().then(() => setNotified(true));
   };
 
   return (
     <>
-      <Button {...buttonProps} onClick={handleOnClick}>
+      <Button
+        {...buttonProps}
+        onClick={handleOnClick}
+        disabled={loading || notified}
+      >
         {buttonLabel}
+        {notified && <FaCheck className="ml-2" />}
       </Button>
     </>
   );
